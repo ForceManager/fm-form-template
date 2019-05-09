722118f4b65de46d2da90cdcae53d0040479ac0e
Sergi Palau <sergi.palau@forcemanager.net>
2019-05-09 16:01:22 +0200

    project start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HoiPoiProvider } from 'hoi-poi-ui';
+import FormSelector from '../components/FormSelector;
+import schema from './schema.json';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HoiPoiProvider>
+        <FormsSelector schema={schema} />
+      </HoiPoiProvider>
     </div>
   );
 }
